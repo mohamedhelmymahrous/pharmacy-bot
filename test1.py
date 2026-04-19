95bb2c7567167e91776c725314ad6f4b2dab4130
Mohamed Helmy <mohamed.helmy.mahrous@gmail.com>
2026-04-19 12:55:06 +0200

    initial commit

---
+++ test1.py
@@ -0,0 +1,13 @@
+print("i am working now")
+name = input(":What is your name")
+print("welcome"+ name)
+age = input("how old are you")
+print(age +"years old")
+if int(age)>= 18:
+ print("you can enter")
+else:
+ print("get out of here")
+ for i in range(3):
+  print("hello my friend")
+for i in range (9):
+  print(i)
